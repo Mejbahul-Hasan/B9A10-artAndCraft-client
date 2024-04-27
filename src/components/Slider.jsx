439a16ae42e8be5f24da5b05b3583f14a1db966e
Mejbahul-Hasan <mejba.du@gmail.com>
2024-04-28 01:54:06 +0600

    Slider added in the home page.

---
+++ src/components/Slider.jsx
@@ -0,0 +1,37 @@
+
+const Slider = () => {
+    return (
+        <div className="carousel w-full max-h-96">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/KKsRkZD/3-birmingham-museums-trust-HEEv-Yh-Nzp-Eo-unsplash.jpg" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/BqwBt0P/4-lucas-k-w-QLAGv4-OYs-unsplash.jpg" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/g6bp7kM/6-erik-mclean-8-Se-JUmfahu0-unsplash.jpg" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/KyNfqf0/5-birmingham-museums-trust-g-Hkgq-Zh-HPb-M-unsplash.jpg" className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { FcRating } from "react-icons/fc";
+import Slider from "../../components/Slider";
 
 const Home = () => {
 
@@ -7,28 +8,31 @@ const Home = () => {
 
     return (
         <>
+            <div className="my-5">
+            <Slider />
+            </div>
             <div>
                 <h1>Total Data: {cardItems.length}</h1>
             </div>
             <div className="lg:grid lg:grid-cols-3 gap-10">
-            {
-                cardItems.map(item =>
-                    <div key={item._id} className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src={item.image} alt="artAndCraft" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                {item.item_name}
-                                <div className="badge badge-secondary">${item.price}</div>
-                            </h2>
-                            <div className="card-actions">
-                                <div className="badge badge-outline">{item.subcategory_name}</div>
-                                <div className="badge badge-outline"><FcRating />{item.rating}</div>
+                {
+                    cardItems.map(item =>
+                        <div key={item._id} className="card w-96 bg-base-100 shadow-xl">
+                            <figure><img src={item.image} alt="artAndCraft" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    {item.item_name}
+                                    <div className="badge badge-secondary">${item.price}</div>
+                                </h2>
+                                <div className="card-actions">
+                                    <div className="badge badge-outline">{item.subcategory_name}</div>
+                                    <div className="badge badge-outline"><FcRating />{item.rating}</div>
+                                </div>
+                                <p>{item.description}</p>
+                                <Link to={`/view-details/${item._id}`}><button className="btn btn-sm w-full">View Details</button></Link>
                             </div>
-                            <p>{item.description}</p>
-                            <Link to={`/view-details/${item._id}`}><button className="btn btn-sm w-full">View Details</button></Link>
-                        </div>
-                    </div>)
-            }
+                        </div>)
+                }
             </div>
         </>
     );
